caf79704b68592ca5fc3bd0d1e3bd60210d37a8e
Анна <annakiseliova7@gmail.com>
2022-09-27 17:54:24 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+       //Условный оператор. Часть 2. Задание 1
+        System.out.println("Условный оператор. Часть 2. Задание 1");
+        int clientOS = 0;
+        if(clientOS == 0){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if(clientOS == 1){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        //Условный оператор. Часть 2. Задание 2
+
+
+
+
+    }
+}
