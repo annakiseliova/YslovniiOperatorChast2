32056e0fe914f5fc483360df3ad6c7f8849b4d4b
Анна <annakiseliova7@gmail.com>
2022-09-27 18:10:51 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-       //Условный оператор. Часть 2. Задание 1
-        System.out.println("Условный оператор. Часть 2. Задание 1");
-        int clientOS = 0;
+       //Условный оператор. Часть 2. Задание 2
+        System.out.println("Условный оператор. Часть 2. Задание 2");
+        int clientOS = 1;
+        int yearOfIssue = 2008;
         if(clientOS == 0){
-            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (yearOfIssue >= 2015 && yearOfIssue <= 2022){
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else if(yearOfIssue < 2015){
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else{
+                System.out.println("Такой год еще не наступил");
+            }
         } else if(clientOS == 1){
-            System.out.println("Установите версию приложения для Android по ссылке");
-        }
+            if (yearOfIssue >= 2015 && yearOfIssue <= 2022){
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else if(yearOfIssue < 2015){
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Такой год еще не наступил");
+            }
 
-        //Условный оператор. Часть 2. Задание 2
+        }
 
 
 
