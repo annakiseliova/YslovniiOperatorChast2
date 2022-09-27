06d934034cf84d94f1095dda1ab0aa3481a7ba10
Анна <annakiseliova7@gmail.com>
2022-09-27 18:28:12 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-       //Условный оператор. Часть 2. Задание 2
-        System.out.println("Условный оператор. Часть 2. Задание 2");
-        int clientOS = 1;
-        int yearOfIssue = 2008;
-        if(clientOS == 0){
-            if (yearOfIssue >= 2015 && yearOfIssue <= 2022){
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            } else if(yearOfIssue < 2015){
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else{
-                System.out.println("Такой год еще не наступил");
-            }
-        } else if(clientOS == 1){
-            if (yearOfIssue >= 2015 && yearOfIssue <= 2022){
-                System.out.println("Установите версию приложения для Android по ссылке");
-            } else if(yearOfIssue < 2015){
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+       //Условный оператор. Часть 2. Задание 3
+        System.out.println("Условный оператор. Часть 2. Задание 3");
+        int year = 1900;
+        boolean bigYear = (year % 100 == 0 && year % 400 == 0);
+        if(year % 4 == 0) {
+            if (bigYear) {
+                System.out.println( year + " год является високосным");
             } else {
-                System.out.println("Такой год еще не наступил");
+                System.out.println(year + " год не является високосным");
             }
-
+        } else {
+            System.out.println(year + " год не является високосным");
         }
 
 
