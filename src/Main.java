2e7e352fa090dc0f511b3ab07cbfa987b37de893
Анна <annakiseliova7@gmail.com>
2022-09-28 17:08:20 +0300

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        //Условный оператор. Часть 2. Задание 4
-        System.out.println("Условный оператор. Часть 2. Задание 4");
-        int deliveryDistance = 95;
-        int dayDelivery = 1;
-        if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: " + dayDelivery);
-        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            dayDelivery = dayDelivery + 1;
-            System.out.println("Потребуется дней: " + dayDelivery);
-        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
-            dayDelivery = dayDelivery + 2;
-            System.out.println("Потребуется дней: " + dayDelivery);
-        }else{
-            System.out.println("Потребуется более 3 дней");
+        //Условный оператор. Часть 2. Задание 5
+        System.out.println("Условный оператор. Часть 2. Задание 5");
+        int monthNomber = 12;
+
+        switch (monthNomber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Не существует такой месяц");
         }
+
     }
 }
