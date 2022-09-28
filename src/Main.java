8cc852f9fbb63d26695cfb3a97b944c6c371f4c1
Анна <annakiseliova7@gmail.com>
2022-09-28 16:52:29 +0300

    Задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-       //Условный оператор. Часть 2. Задание 3
-        System.out.println("Условный оператор. Часть 2. Задание 3");
-        int year = 1900;
-        boolean bigYear = (year % 100 == 0 && year % 400 == 0);
-        if(year % 4 == 0) {
-            if (bigYear) {
-                System.out.println( year + " год является високосным");
-            } else {
-                System.out.println(year + " год не является високосным");
-            }
-        } else {
-            System.out.println(year + " год не является високосным");
+        //Условный оператор. Часть 2. Задание 4
+        System.out.println("Условный оператор. Часть 2. Задание 4");
+        int deliveryDistance = 95;
+        int dayDelivery = 1;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + dayDelivery);
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            dayDelivery = dayDelivery + 1;
+            System.out.println("Потребуется дней: " + dayDelivery);
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            dayDelivery = dayDelivery + 2;
+            System.out.println("Потребуется дней: " + dayDelivery);
+        }else{
+            System.out.println("Потребуется более 3 дней");
         }
-
-
-
-
     }
 }
